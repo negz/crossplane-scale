d294846b1a68c8b6b7563a92499aeeaa85b5082c
Nic Cope <negz@rk0n.org>
2021-10-29 09:21:39 +0000

    Make CRD scale testing script more tunable
    Signed-off-by: Nic Cope <negz@rk0n.org>

--- etoomanycrds/create.sh
+++ etoomanycrds/create.sh
@@ -3,22 +3,29 @@
 
 set +e
 
-if [[ -z ${2} ]]; then
-        echo "Usage: $0 <sleep-seconds> <batches>"
+if [[ -z ${3} ]]; then
+        echo "Usage: $0 <sleep-seconds> <batches> <batch-size>"
         exit 1
 fi
 
 SLEEP=${1}
 COUNT=$((${2}-1))
+SIZE=$((${3}-1))
+
+function batch {
+        local j=0
+        while [ ${j} -le ${SIZE} ]; do
+                sed "s/%GROUP%/${RANDOM}.example.org/g" crd.yaml.tmpl
+                j=$((j+1))
+        done
+}
 
 i=0
 while [ ${i} -le ${COUNT} ]; do
-        group=${RANDOM}.example.org
-
-        sed "s/%GROUP%/${group}/g" 100-crds.yaml | kubectl apply -f -
+        batch | kubectl apply -f -
 
         echo
-        echo "Iteration ${i} complete"
+        echo "Iteration $((i+1)) complete"
         echo
 
         sleep ${SLEEP}
